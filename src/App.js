6fc994a34d6c5f3a9c8a53d8f1ae67281cc4075e
ravikrushna963 <ravikrushna963@gmail.com>
2022-09-27 18:45:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [resStatusCode, setResStatusCode] = useState("");
+  const [resData, setResData] = useState("");
+  const [status, setStatus] = useState(false);
+  const [showData, setShowData] = useState(false);
+
+  useEffect(() => {
+    let options = {
+      method: "GET",
+    };
+    fetch("https://gorest.co.in/public-api/users", options)
+      .then(function (response) {
+        setResStatusCode(response.status);
+        return response.text();
+      })
+      .then(function (Data) {
+        console.log(Data);
+        setResData(Data);
+      });
+  }, [status]);
+
+  const onClickButton = () => {
+    setStatus(!status);
+    setShowData(!showData);
+  };
+
+  const reqUrl = " https://gorest.co.in/public-api/users";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="p-3 bg-container">
+      <h1 className="heading mb-4">Get method practice</h1>
+      <div className="request-url-text">
+        REQUEST URL :
+      </div>
+      <input type="text" className="form-control" value={reqUrl} placeholder="Enter URL" />
+      <button
+        id="sendGetRequestBtn"
+        className="mt-3 p-2 button"
+        onClick={onClickButton}
+      >
+        Send Get Request
+      </button>
+      <hr />
+      <div className="p-2 mt-2">
+        <p className="para1">Request Status</p>
+        <p id="requestStatus" className="request-status">
+          {showData && resStatusCode}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <hr />
+      <div className="p-2 mt-2">
+        <p className="para1">Response Body</p>
+        <p id="httpResponse" className="http-response">
+          {showData && resData}
+        </p>
+      </div>
     </div>
   );
 }
